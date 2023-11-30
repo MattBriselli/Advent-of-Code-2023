ab9dbb6cc5dd29b09f237a5cfe950ca59ca35825
mattbriselli <mattbriselli@gmail.com>
2023-11-29 16:58:15 -0800

    added

---
+++ Day1/Day1.java
@@ -0,0 +1,22 @@
+package main.java.org.adventofcode2023.Day1;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Day1 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("/Users/M/IdeaProjects/Advent of Code 2023/src/main/java/org/adventofcode2023/Day1/List"));
+
+        int total = 0;
+        iterate(reader);
+
+        System.out.println(total);
+    }
+
+    private static void iterate(BufferedReader reader) {
+        return;
+    }
+}
