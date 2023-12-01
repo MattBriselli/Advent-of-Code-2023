3b0394d225f65fc93a652ca59c6d31641ad1c4d0
mattbriselli <mattbriselli@gmail.com>
2023-11-30 20:30:36 -0800

    boilerplate day1:

--- Day1/Day1.java
+++ Day1/Day1.java
@@ -4,19 +4,30 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Day1 {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("/Users/M/IdeaProjects/Advent of Code 2023/src/main/java/org/adventofcode2023/Day1/List"));
+//        BufferedReader reader = new BufferedReader(new FileReader("/Users/M/IdeaProjects/Advent of Code 2023/src/main/java/org/adventofcode2023/Day1/List"));
+        BufferedReader testReader = new BufferedReader(new FileReader("/Users/M/IdeaProjects/Advent of Code 2023/src/main/java/org/adventofcode2023/Day1/Test"));
 
         int total = 0;
-        iterate(reader);
+        total = iterate(testReader, total);
 
         System.out.println(total);
     }
 
-    private static void iterate(BufferedReader reader) {
-        return;
+    private static int iterate(BufferedReader reader, int total) throws IOException {
+        ArrayList<Integer> list = new ArrayList<>();
+        String line = reader.readLine();
+
+        while (line != null) {
+            list.add(Integer.parseInt(line));
+            line = reader.readLine();
+            total++;
+        }
+
+        return total;
     }
 }
