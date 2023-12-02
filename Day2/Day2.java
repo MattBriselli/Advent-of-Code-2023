aba9d6c15b4efe9d3caa01e4f3d253cfe358abde
mattbriselli <mattbriselli@gmail.com>
2023-12-01 21:30:46 -0800

    working day2

--- Day2/Day2.java
+++ Day2/Day2.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.Logger;
 
 public class Day2 {
 
@@ -17,14 +18,69 @@ public class Day2 {
     }
 
     private static int iterate(BufferedReader reader) throws IOException {
-        ArrayList<String> list = new ArrayList<>();
         String line = reader.readLine();
-        int total = 0;
+
+        int minRed = -1;
+        int minGreen = -1;
+        int minBlue = -1;
+
+        int redStock = 0;
+        int greenStock = 0;
+        int blueStock = 0;
+        int gameTotal = 0;
 
         // Do stuff
 
-        line = reader.readLine();
+        while (line != null) {
+            System.out.println(line);
+            String[] rounds = line.split(":")[1].split(",");
 
-        return total;
+            for (String round : rounds) {
+                String[] games = round.split(";");
+                for (String game : games) {
+                    game = game.trim();
+                    int number = Integer.parseInt(game.split(" ")[0]);
+                    String color = game.split(" ")[1];
+                    switch (color) {
+                        case "blue":
+                        case "blue,":
+                            blueStock += number;
+                            break;
+                        case "red":
+                        case "red,":
+                            redStock += number;
+                            break;
+                        case "green":
+                        case "green,":
+                            greenStock += number;
+                            break;
+                    }
+                    System.out.println(blueStock + " : " + greenStock + " : " + redStock);
+                    if (minBlue < blueStock) {
+                        minBlue = blueStock;
+                    }
+                    if (minRed < redStock) {
+                        minRed = redStock;
+                    }
+                    if (minGreen < greenStock) {
+                        minGreen = greenStock;
+                    }
+
+                    redStock = 0;
+                    greenStock = 0;
+                    blueStock = 0;
+                }
+            }
+            System.out.println(minBlue + " : " + minGreen + " : " + minRed);
+            gameTotal += (minBlue * minGreen * minRed);
+
+            minBlue = 0;
+            minRed = 0;
+            minGreen = 0;
+
+            line = reader.readLine();
+        }
+
+        return gameTotal;
     }
 }
