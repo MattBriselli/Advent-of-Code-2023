17930bb80abbeed8c6ae2a5c23102b177930a0a8
mattbriselli <mattbriselli@gmail.com>
2023-12-02 22:25:36 -0800

    working day3 pt 2

--- Day3/Day3.java
+++ Day3/Day3.java
@@ -20,41 +20,94 @@ public class Day3 {
     private static int iterate(BufferedReader reader) throws IOException {
         String line = reader.readLine();
         int sum = 0;
-        int ticker = 0;
 
         char[][] board = new char[line.length()][];
 
+        int ticker = 0;
         while (line != null) {
             board[ticker] = line.toCharArray();
             ticker++;
             line = reader.readLine();
         }
+
         for (int i = 0; i < board.length; i++) {
-            StringBuilder wordNum = new StringBuilder();
-
-            for (int j = 0; j < board[i].length; j++) {
+            for (int j = 0; j < board[0].length; j++) {
                 char c = board[i][j];
-                int newJ = j;
-
-                if (Character.isDigit(c)) {
-                    while (newJ < board[i].length && Character.isDigit(board[i][newJ])) {
-                        wordNum.append(board[i][newJ]);
-                        newJ++;
-                    }
+                if (c == '*') {
+                    System.out.println("found a * at: " + i + ", " + j);
+                    sum += grabSurrounding(board, i, j);
                 }
-                if (!wordNum.isEmpty()) {
-                    System.out.println(wordNum);
-                    int num = Integer.parseInt(wordNum.toString());
-                    if (connectedToChar(num, board, i, j, newJ)) {
-                        sum += num;
+            }
+
+        }
+//        Part 1
+//        for (int i = 0; i < board.length; i++) {
+//            StringBuilder wordNum = new StringBuilder();
+//
+//            for (int j = 0; j < board[i].length; j++) {
+//                char c = board[i][j];
+//                int newJ = j;
+//
+//                if (Character.isDigit(c)) {
+//                    while (newJ < board[i].length && Character.isDigit(board[i][newJ])) {
+//                        wordNum.append(board[i][newJ]);
+//                        newJ++;
+//                    }
+//                }
+//                if (!wordNum.isEmpty()) {
+//                    System.out.println(wordNum);
+//                    int num = Integer.parseInt(wordNum.toString());
+//                    if (connectedToChar(num, board, i, j, newJ)) {
+//                        sum += num;
+//                    }
+//                    System.out.println("New sum is: " + sum);
+//                    j = newJ;
+//                    wordNum = new StringBuilder();
+//                }
+//            }
+//        }
+        return sum;
+    }
+
+    private static int grabSurrounding(char[][] board, int row, int col) {
+        int total = 1;
+        int found = 0;
+
+        int maxX = col == board[0].length ? col : col + 1;
+        int minX = col == 0 ? col : col -1;
+        int minY = row == 0 ? 0 : row - 1;
+        int maxY = row == board.length ? row : row + 1;
+
+        if (maxX == board[0].length) {
+            maxX--;
+        }
+
+        for (int i = minY; i <= maxY; i++) {
+            for (int j = minX; j <= maxX; j++) {
+                char c = board[i][j];
+                if (Character.isDigit(c)) {
+                    StringBuilder numString = new StringBuilder(Character.toString(c));
+                    int tempJ = j - 1;
+                    while(tempJ >= 0 && Character.isDigit(board[i][tempJ])) {
+                        numString = new StringBuilder(Character.toString(board[i][tempJ])).append(numString);
+                        tempJ--;
                     }
-                    System.out.println("New sum is: " + sum);
-                    j = newJ;
-                    wordNum = new StringBuilder();
+                    tempJ = j + 1;
+                    while(tempJ < board.length && Character.isDigit(board[i][tempJ])) {
+                        numString = new StringBuilder(numString).append(Character.toString(board[i][tempJ]));
+                        tempJ++;
+                    }
+                    j = tempJ;
+                    total *= Integer.parseInt(numString.toString());
+                    found++;
                 }
             }
         }
-        return sum;
+
+        if (found < 2) {
+            total = 0;
+        }
+        return total;
     }
 
     private static boolean connectedToChar(int num, char[][] board, int row, int startCol, int endCol) {
