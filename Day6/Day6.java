61698175c90a50670a73b0b1c123fcfda9b646cf
mattbriselli <mattbriselli@gmail.com>
2023-12-05 21:15:43 -0800

    pt 1 done

--- Day6/Day6.java
+++ Day6/Day6.java
@@ -3,26 +3,63 @@ package main.java.org.adventofcode2023.Day6;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Day6 {
 
     public static void main(String[] args) throws IOException {
-        //        BufferedReader reader = new BufferedReader(new FileReader("/Users/M/IdeaProjects/Advent of Code 2023/src/main/java/org/adventofcode2023/Day6/List"));
-        BufferedReader reader = new BufferedReader(new FileReader("/Users/M/IdeaProjects/Advent of Code 2023/src/main/java/org/adventofcode2023/Day6/Test"));
+                BufferedReader reader = new BufferedReader(new FileReader("/Users/M/IdeaProjects/Advent of Code 2023/src/main/java/org/adventofcode2023/Day6/List"));
+//        BufferedReader reader = new BufferedReader(new FileReader("/Users/M/IdeaProjects/Advent of Code 2023/src/main/java/org/adventofcode2023/Day6/Test"));
 
         System.out.println(iterate(reader));
     }
 
     private static int iterate(BufferedReader reader) throws IOException {
         String line = reader.readLine();
-        int sum = 0;
+        List<Integer> timeList = new ArrayList<>();
+        List<Integer> distanceList = new ArrayList<>();
+        List<Integer> winners = new ArrayList<>();
 
         while (line != null) {
-            // do stuff
+            System.out.println(line);
+            String title = line.split(":")[0];
+            String values = line.split(":")[1].trim();
 
+            String[] valueList = values.split(" ");
+            for (String v : valueList) {
+                if (!v.isBlank()) {
+                    if (title.equals("Time")) {
+                        timeList.add(Integer.parseInt(v));
+                    } else {
+                        distanceList.add(Integer.parseInt(v));
+                        winners.add(0);
+                    }
+                }
+            }
             line = reader.readLine();
         }
 
-        return sum;
+        for (int i = 0; i < timeList.size(); i++) {
+            int time = timeList.get(i);
+            int distance = distanceList.get(i);
+
+            for (int j = 0; j < time; j++) {
+                int speed = (time - j);
+                int turnDistance = speed * j;
+                System.out.println(turnDistance);
+                if (turnDistance > distance) {
+                    winners.set(i, winners.get(i) + 1);
+                }
+            }
+        }
+
+        int total = 1;
+        for (int w : winners) {
+            System.out.println("winner: " + w);
+            total *= w;
+        }
+
+        return total;
     }
 }
