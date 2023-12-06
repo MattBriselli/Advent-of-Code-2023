75bc507bd737d15674b53c8406bdd665fd468aae
mattbriselli <mattbriselli@gmail.com>
2023-12-05 18:26:24 -0800

    day 6 boiler

---
+++ Day6/Day6.java
@@ -0,0 +1,28 @@
+package main.java.org.adventofcode2023.Day6;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Day6 {
+
+    public static void main(String[] args) throws IOException {
+        //        BufferedReader reader = new BufferedReader(new FileReader("/Users/M/IdeaProjects/Advent of Code 2023/src/main/java/org/adventofcode2023/Day6/List"));
+        BufferedReader reader = new BufferedReader(new FileReader("/Users/M/IdeaProjects/Advent of Code 2023/src/main/java/org/adventofcode2023/Day6/Test"));
+
+        System.out.println(iterate(reader));
+    }
+
+    private static int iterate(BufferedReader reader) throws IOException {
+        String line = reader.readLine();
+        int sum = 0;
+
+        while (line != null) {
+            // do stuff
+
+            line = reader.readLine();
+        }
+
+        return sum;
+    }
+}
