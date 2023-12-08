428cb5addeb2bfb0b47b2ad8a76191474444de8c
mattbriselli <mattbriselli@gmail.com>
2023-12-07 20:48:59 -0800

    day8 boiler and x tweak

--- Day8/Day8.java
+++ Day8/Day8.java
@@ -1,14 +1,14 @@
-package main.java.org.adventofcode2023;
+package main.java.org.adventofcode2023.Day8;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class DayX {
+public class Day8 {
 
     public static void main(String[] args) throws IOException {
-//        BufferedReader reader = new BufferedReader(new FileReader("/Users/M/IdeaProjects/Advent of Code 2023/src/main/java/org/adventofcode2023/DayX/List"));
-        BufferedReader reader = new BufferedReader(new FileReader("/Users/M/IdeaProjects/Advent of Code 2023/src/main/java/org/adventofcode2023/DayX/Test"));
+//        BufferedReader reader = new BufferedReader(new FileReader("/Users/M/IdeaProjects/Advent of Code 2023/src/main/java/org/adventofcode2023/Day8/List"));
+        BufferedReader reader = new BufferedReader(new FileReader("/Users/M/IdeaProjects/Advent of Code 2023/src/main/java/org/adventofcode2023/Day8/Test"));
 
         System.out.println(iterate(reader));
     }
